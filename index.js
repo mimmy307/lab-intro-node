191aeb091cd34654d6e0fc8ada14a6ef5f6a40a2
“Mynorka <mynorkadaza@gmail.com>
2024-04-25 21:21:48 +0200

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [ ];
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+     
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.items.length){
+      throw new Error("OutOfBounds");
+    }
 
-  max() {}
+    return this.items[pos]
+  }
 
-  min() {}
+  max() {
+    if (this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items)
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
 
-  avg() {}
+  }
+
+  sum() {
+    if (this.items.length === 0){
+      return 0
+    }
+    return this.items.reduce((accumulator, currentValue) => accumulator + currentValue)
+  }
+
+  avg() {
+    if (this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.items.reduce(function(accumulator, currentValue){ 
+      return accumulator + currentValue;
+    })/this.items.length;
+   
+}
 }
 
 module.exports = SortedList;
